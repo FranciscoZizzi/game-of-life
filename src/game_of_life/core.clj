094258a24c0ce8a795f0c549bc8dc217d4e2cc59
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-12-12 20:49:15 -0300

    basic core

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -1,7 +1,13 @@
 (ns game-of-life.core
-  (:gen-class))
+  (:gen-class)
+  (:require [game-of-life.utils :as utils]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn create-board 
+  [width height]
+  {:width width :height height :cells #{}})
+
