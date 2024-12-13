a75bb82903cb845852d2b427debad8284068d131
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-12-13 13:37:30 -0300

    create test framework

---
+++ test/game_of_life/test_utils.clj
@@ -0,0 +1,41 @@
+(ns game-of-life.test-utils
+  (:require [game-of-life.core :as core]
+            [clojure.test :refer [is]]))
+
+(defn- board-to-matrix
+  [board]
+  (let [cells (:cells board)]
+    (loop [x 0
+           y 0
+           matrix []]
+      (if (>= y (:height board))
+        matrix
+        (let [row (or (get matrix y []) [])
+              next-matrix (assoc matrix y (if (contains? cells [x y])
+                             (conj row 'o)
+                             (conj row '-)))
+             next-x (mod (inc x) (:width board))
+             next-y (if (zero? next-x) (inc y) y)]
+         (recur next-x next-y next-matrix)))
+     )))
+
+(defn- matrix-to-board
+  [matrix]
+  (loop [x 0
+         y 0
+         cells #{}
+         width (count (get matrix 0))
+         height (count matrix)]
+    (if (>= y height)
+      {:height height :width width :cells cells}
+      (let [new-cells (if (= (get-in matrix [x y]) 'o)
+                        (conj cells [x y])
+                        cells)
+            next-x (mod (inc x) width)
+            next-y (if (= next-x 0) (inc y) y)]
+        (recur next-x next-y new-cells width height)))))
+
+(defn expect-next-state
+  [current expected]
+  (is (= (board-to-matrix (core/next-tick (matrix-to-board current))) expected)))
+
