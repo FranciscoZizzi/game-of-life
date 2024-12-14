195c1e519b7036defa082d5c33af5ee529cef0b6
FranciscoZizzi <fzizzi@mail.austral.edu.ar>
2024-12-14 01:25:30 -0300

    setting up cljfx dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Conway's Game of Life"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [cljfx "1.9.2"]]
   :main game-of-life.main
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
